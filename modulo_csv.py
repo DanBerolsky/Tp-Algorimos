3bcce9e601a65937f3eea573900793641b1e4ea6
Alfonso Campodonico <alfonso.campodonico@gmail.com>
2020-07-06 15:50:39 -0300

    modulo csv

--- modulo_csv.py
+++ modulo_csv.py
@@ -1,45 +1,4 @@
 def armo_csv(dic_ordenado,nombre_archivo):
-<<<<<<< HEAD
-    if nombre_archivo == "fuente_unico.csv":
-    
-        with open (nombre_archivo,"a") as codigo:
-
-            # codigo.write("Nombre de la funcion Parametros formales Nombre del modulo")
-
-            for clave in dic_ordenado:
-
-                nombre_funcion = clave[0]
-                parametros = clave[1][0]
-                modulo = clave[1][1]
-                cuerpo = clave[1][2]
-
-                funcion = ", ".join(cuerpo)
-
-                # codigo.write(nombre_funcion+",\t\t\t\t\t\t"+parametros+",\t\t\t\t"+modulo+",\n\n"+"Cuerpo de la funcion:\n\n"+funcion+"\n\n")
-
-                codigo.write(nombre_funcion+","+parametros+","+modulo+","+funcion+"\n")
-
-                """codigo.write(nombre_funcion+","+parametros+","+modulo+","+"\n\n"+funcion+"\n\n") ESTE LO CARGA MAS LIMPIO"""
-    elif nombre_archivo == "comentarios.csv":
-          with open (nombre_archivo,"a") as codigo:
-                for clave in dic_ordenado:
-                    nombre_funcion = clave[0]
-                    autor = clave[1][0]
-                    ayuda = clave[1][1]
-                    cuerpo = clave[1][2]
-                    funcion = ", ".join(cuerpo)
-
-                    # codigo.write(nombre_funcion+",\t\t\t\t\t\t"+parametros+",\t\t\t\t"+modulo+",\n\n"+"Cuerpo de la funcion:\n\n"+funcion+"\n\n")
-
-                    codigo.write(nombre_funcion+","+autor+","+ayuda+","+funcion+"\n")
-                    
-
-                    
-
-                    
-    else:
-        print("Como llegamos aca. Ayuda")
-=======
     """[Autor: F] """
     """[Ayuda: Hace cosas] """
     with open (nombre_archivo,"a") as codigo:
@@ -52,4 +11,10 @@ def armo_csv(dic_ordenado,nombre_archivo):
             funcion = ", ".join(cuerpo)
             #Escribo en el csv
             codigo.write(nombre_funcion+","+parametros+","+modulo+","+funcion+"\n")
->>>>>>> feature/arregloschicos
+        return None
+def csv_existen(nombre_archivo):
+    try:
+        my_file = open(nombre_archivo)
+        return False
+    except IOError:
+        return True
