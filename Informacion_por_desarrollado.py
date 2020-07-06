5b40b747c5022d5abba3772fbd2be8b47864b826
Dan-Desktop <dan.bero15@gmail.com>
2020-07-06 17:53:45 -0300

    fucion de info de desarrolladores (terminada y funcionando con datos de ejemplo)

--- Informacion_por_desarrollado.py
+++ Informacion_por_desarrollado.py
@@ -1,7 +1,3 @@
-import muestro_salida
-
-datos_prueba = [("Luis","leer",10,26),("Luis","imprimir",10,26),("Dan","participacion_info",55,73)]
-
 def participacion_info (informacion):
     
     """[Autor: c]"""
@@ -10,7 +6,11 @@ def participacion_info (informacion):
     autor_anterior = None
     total_funciones = 0
     muestro_salida.impresiones("\n\tInforme de Desarrollo Por Autor\n")
-
+    
+    #
+    archivo_participacion_txt("\n\tInforme de Desarrollo Por Autor\n")
+    #
+    
     for indice in range(len(informacion)):     
         
         autor, nombre_funcion, lineas_funcion, porcentaje = informacion[indice]
@@ -19,12 +19,16 @@ def participacion_info (informacion):
 
         if autor_anterior == None:
 
-            muestro_salida.impresiones("Autor: " + autor +"\n\n\tFuncion\t\t\tLineas\n\t---------------------------------\n")
+            muestro_salida.impresiones("Autor: " + autor +"\n\n\tFuncion\t\t\tLineas\n\t---------------------------------")
             
             espacios = 8 + len(nombre_funcion) - 32
             espacios1 = " "*(-1*espacios)
             muestro_salida.impresiones("\t" + nombre_funcion + espacios1 + str(lineas_funcion))
             
+            #
+            archivo_participacion_txt("\nAutor: " + autor +"\n\n\tFuncion\t\t\tLineas\n\t---------------------------------")
+            archivo_participacion_txt("\n\t" + nombre_funcion + espacios1 + str(lineas_funcion))
+            #
 
             contador_lineas_totales = 0
             
@@ -42,12 +46,18 @@ def participacion_info (informacion):
             
             muestro_salida.impresiones("\t"+str(contador_funciones) + " Funciones - Lineas\t" + str(contador_lineas) + "  " + str(porcentaje_anterior)+"%\n")
             
-            muestro_salida.impresiones("Autor: " + autor + "\n")
+            muestro_salida.impresiones("Autor: " + autor +"\n\n\tFuncion\t\t\tLineas\n\t---------------------------------")
             
             espacios = 8 + len(nombre_funcion) - 32
             espacios1 = " "*(-1*espacios)
             muestro_salida.impresiones("\t" + nombre_funcion + espacios1 + str(lineas_funcion))
             
+            #
+            archivo_participacion_txt("\n\t"+str(contador_funciones) + " Funciones - Lineas\t" + str(contador_lineas) + "  " + str(porcentaje_anterior)+"%\n")
+            archivo_participacion_txt("\nAutor: " + autor +"\n\n\tFuncion\t\t\tLineas\n\t---------------------------------")
+            archivo_participacion_txt("\n\t" + nombre_funcion + espacios1 + str(lineas_funcion))
+            #
+
             contador_funciones = 0
 
             contador_lineas = 0
@@ -59,8 +69,9 @@ def participacion_info (informacion):
             espacios = 8 + len(nombre_funcion) - 32
             espacios1 = " "*(-1*espacios)
             muestro_salida.impresiones("\t" + nombre_funcion + espacios1 + str(lineas_funcion))
-
-        
+            #
+            archivo_participacion_txt("\n\t" + nombre_funcion + espacios1 + str(lineas_funcion))
+            #
 
         contador_lineas_totales += int(lineas_funcion)
 
@@ -76,10 +87,25 @@ def participacion_info (informacion):
             
             muestro_salida.impresiones("\t"+str(contador_funciones) + " Funciones - Lineas\t" + str(contador_lineas) + "  " + str(porcentaje)+"%\n\n")
             muestro_salida.impresiones("Total: "+ str(contador_funciones_totales) + " Funciones - lineas\t" + str(contador_lineas_totales)+"\n")
-            
+
+            archivo_participacion_txt("\n\t"+str(contador_funciones) + " Funciones - Lineas\t" + str(contador_lineas) + "  " + str(porcentaje)+"%\n\n")
+            archivo_participacion_txt("\nTotal: "+ str(contador_funciones_totales) + " Funciones - lineas\t" + str(contador_lineas_totales)+"\n")            
 
 
 
+def archivo_participacion_txt (agrego):
+    
+    """[Autor: c]"""
+    """[Ayuda: abre un archivo]"""
 
+    with open ("participacion.txt","a") as archivo_generado:
+        
+        archivo_generado.write(agrego)
+
+# =============================================================================
+
+import muestro_salida
+
+datos_prueba = [("Luis","leer",55,36),("Luis","imprimir",10,30),("Dan","participacion_info",55,30),("alfonso","leer_py",60,33)]
 
 participacion_info(datos_prueba)
