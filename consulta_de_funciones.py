642369209b9eca92b7d63e10f6d3c084a50a690b
valentinthourte <vthourte@fi.uba.ar>
2020-07-12 20:37:18 -0300

    Modulo actualizado y terminado(casi)

--- consulta_de_funciones.py
+++ consulta_de_funciones.py
@@ -1,3 +1,9 @@
+"""info_imprimir = "Nombre de la función: " + clave1 + " " + "Parametros: " + str(dict_fuente[clave1][0]) + "" + "Modulo: " + str(dict_fuente[clave1][1]) + " " + "Autor: " + dict_comentarios[clave1][0] + " " + "Ayuda: " + sacar_corchetes(str(dict_comentarios[clave1][1])) + " " + "Cuerpo: " + str(dict_fuente[clave1][2]) + " " + "Comentarios: " + str(dict_comentarios[clave1][2]) + "\n\n "
+
+
+"Nombre de la función: " + clave1 + " " + "Parametros: " + str(dict_fuente[clave1][0]) + " " + "Modulo: " + str(dict_fuente[clave1][1]) + " " + "Autor: No especificado" + " " + "Ayuda: No especificada" + " " + "Cuerpo: " + str(dict_fuente[clave1][2]) + " " + "\n\n")
+"""
+
 def armar_diccionarios():
     """[Autor: Valentin]"""
     """[Ayuda: Imprime la lista de funciones, y da la opción de mostrar información acerca de cada una.]"""
@@ -29,9 +35,6 @@ def armar_diccionarios():
         return diccionario_fuente_unico, diccionario_comentarios
 
 
-
-"""clave1 + "Parametros: " + dict_fuente[clave1][0] + "Modulo: " + dict_fuente[clave1][0] + "Autor: " + dict_comentarios[clave2][0] + """
-
 def sacar_corchetes(cadena):
     """[Autor: Valentin]
     [Ayuda: Recibe como parametro una cadena y le saca los corchetes de adelante y atrás]"""
@@ -42,30 +45,43 @@ def sacar_corchetes(cadena):
     return sin_corchetes
 
 def generar_txt(dict_fuente, dict_comentarios, txt):
+    diccionario_slices = {}
+    for clave in dict_fuente:
+        if clave in dict_comentarios:
+            indice_anterior = 0
+            diccionario_slices[clave] = []
+            info_imprimir = "Nombre de la función: " + clave + " " + "Parametros: " + str(dict_fuente[clave][0]) + "" + "Modulo: " + str(dict_fuente[clave][1]) + " " + "Autor: " + dict_comentarios[clave][0] + " " + "Ayuda: " + sacar_corchetes(str(dict_comentarios[clave][1])) + " " + "Cuerpo: " + str(dict_fuente[clave][2]) + " " + "Comentarios: " + str(dict_comentarios[clave][2]) + "\n\n "
+            for caracter in range(len(info_imprimir)):
+                if caracter % 80 == 0:
+                    diccionario_slices[clave].append(info_imprimir[indice_anterior:caracter])
+                    indice_anterior = caracter
+            diccionario_slices[clave].append("\n")
+        else:
+            pass
     with open(txt, "w") as archivo:
-        for clave1, clave2 in zip(dict_fuente, dict_comentarios):
-            archivo.write("Nombre de la función: " + clave1 + " " + "Parametros: " + str(dict_fuente[clave1][0]) + " " + "Modulo: " + str(dict_fuente[clave1][1]) + " " + "Autor: " + dict_comentarios[clave2][0] + " " + "Ayuda: " + sacar_corchetes(str(dict_comentarios[clave2][1])) + " " + "Cuerpo: " + str(dict_fuente[clave1][2]) + " " + "Comentarios: " + str(dict_comentarios[clave2][2]) + "\n\n")
-
+        for lista in diccionario_slices:
+            for item in range(len(diccionario_slices[lista])):
+                archivo.write(diccionario_slices[lista][item] + "\n")
 
 def acomodar_txt(archivo):
-    with open(archivo, "r+") as txt:
+    """with open(archivo, "r+") as txt:
+        total_lineas = txt.readlines()
+        cantidad_lineas = total_lineas.count("\n")
+        print(cantidad_lineas)
         linea = txt.readline()
         while linea:
-            cantidad_caracteres = 0
-            for letra in linea:
-                if cantidad_caracteres == 80:
-                    linea_acomodada = linea[:80] + "\n" + linea[80:]
-                    linea = linea_acomodada
-                cantidad_caracteres += 1
-            linea = txt.readline
+            linea_nueva = linea
+            if len(linea) > 80:
+            del total_lineas[:21]
+            linea = txt.readline()"""
+    pass
+
 def generar_lista_total(dic):
     """[Autor: Valentin]"""
     """[Ayuda: Genera una lista de listas con los nombres de las funciones ordenadas alfabeticamente]"""
     lista_total = [[]]
-
     for i in dic:
         ultima_lista = lista_total[-1]
-        lista_format = []
         if len(ultima_lista) < 5:
             ultima_lista.append(format(i, "<26s"))
         else:
@@ -76,6 +92,7 @@ def generar_lista_total(dic):
         for i in range(0, 5-len(lista_total[-1])):
             lista_total[-1].append(format(" ", "<26s"))
     return lista_total
+
 
 def imprimir_funciones(listas):
     """[Autor: Valentin]"""
@@ -103,7 +120,7 @@ def consultar_funciones(diccionario_fuente, diccionario_comentarios):
                         print("Autor: " + sacar_corchetes(diccionario_comentarios[i][0]) + "\n" + "Parametros: " + diccionario_fuente[i][0] + "\n" + "Modulo: " + diccionario_fuente[i][1] + "\n" + "Ayuda: " + sacar_corchetes(diccionario_comentarios[i][1]) + "\n" + "Cuerpo: " + str(diccionario_fuente[i][2]) + "\n" + "Comentarios: " + str(diccionario_comentarios[i][2]))
                         print("\n")
             elif funcion == "imprimir ?todo":
-                print("Dirijase al archivo ayuda_funciones.txt.")
+                pass
         else:
             print("La función especificada no existe. Por favor, ingrese una función valida")
         funcion = input("Función: ")
