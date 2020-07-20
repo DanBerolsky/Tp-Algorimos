a3a20066804d9f84ded1741129b6167f38196f30
Dan-Desktop <dan.bero15@gmail.com>
2020-07-19 23:40:28 -0300

    programa principal funcionando

--- programa_principal.py
+++ programa_principal.py
@@ -5,28 +5,34 @@ def main():
     """[Autor: L]"""
     """[Ayuda: Es la funcion principal]"""
     
-
-    # Primer punto
-    import m_generar_archivos_csv
-    txt = 'programas.txt'
-    m_generar_archivos_csv.armar_csv_funciones(txt)
     
     funcionalidad = input("Ingrese la funcionalidad que quiere ver: ")
+    
     while funcionalidad != "":
-        if funcionalidad == 1:
-            """Integrar punto 1"""
-        elif funcionalidad == 2:
+        
+        if funcionalidad == "1":
+            # Primer punto
+            import m_generar_archivos_csv
+            txt = 'programas.txt'
+            m_generar_archivos_csv.armar_csv_funciones(txt)
+        
+        elif funcionalidad == "2":
+            # Segundo punto
             import consulta_de_funciones
             consulta_de_funciones.main_consulta_funciones()
-        elif funcionalidad == 3:
+        
+        elif funcionalidad == "3":
             """Integrar punto 3"""
-        elif funcionalidad == 4:
+        
+        elif funcionalidad == "4":
             """Integrar punto 4"""
-        elif funcionalidad == 5:
+        
+        elif funcionalidad == "5":
             # Quinto punto
             import Informacion_por_desarrollador
             informacion,porcentaje = Informacion_por_desarrollador.capturo_datos()
             Informacion_por_desarrollador.participacion_info(informacion,porcentaje)
+        
         funcionalidad = input("Ingrese la funcionalidad que quiere ver: ")
 
 
