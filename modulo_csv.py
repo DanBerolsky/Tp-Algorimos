41ed458e40a026a55d12932dcdf65bfdc273136e
Dan-Desktop <dan.bero15@gmail.com>
2020-07-20 00:19:39 -0300

    revisar el generar_archivos_csv

--- modulo_csv.py
+++ modulo_csv.py
@@ -1,12 +1,14 @@
+import merge
 def armo_csv(Estructura_de_datos,nombre_archivo):
-    
     """ [Autor: Dan]
         [Ayuda: Recibo el nombre de archivo a escribir y la estructura de datos 
         correspondiente, la cual viene dada por una lista de tuplas,Crea y agrega 
         informacion dentro del csv] 
     """
 
-    if nombre_archivo.startswith("fuente_unico"):
+    if nombre_archivo == "fuente_unico.csv":
+        
+        lista_modulos_fuente = [[]]
 
         for clave in Estructura_de_datos:
 
@@ -18,33 +20,52 @@ def armo_csv(Estructura_de_datos,nombre_archivo):
             
             # Une con una coma los elementos de la lista, en una cadena nueva.
             funcion = ", ".join(cuerpo)
-            
+
             # Genera un nombre diferente para cada modulo, para despues hacer el merge.
             archivo_a_escribir = nombre_archivo + "_" + modulo + ".csv"
             
+            #------------------------------------------------------------------
+            
+            ya_esta = 0
+            index = 0
+            
+            while ya_esta == 0 and index <= len(lista_modulos_fuente[0]) - 1:
+                
+                if lista_modulos_fuente[0][index] == archivo_a_escribir:
+                    ya_esta = 1
+                index += 1
+            
+            if ya_esta == 0:
+                lista_modulos_fuente[0].append(archivo_a_escribir)
+            
+            #------------------------------------------------------------------
+
             # Crea/abre el csv recivido por parametro.
             with open(archivo_a_escribir, "a") as archivo_fuente_unico:
+                
                 #Escribo en el csv
                 archivo_fuente_unico.write(nombre_funcion+","+parametros+","+modulo+","+funcion+"\n")
-    
+        
+        merge.ciclar_modulos(lista_modulos_fuente)
+
     elif nombre_archivo == 'comentarios.csv':
         # recorro la lista de tuplas y capturo los datos deseados   
         for elementos in Estructura_de_datos:
-                
+
             #Modelo de parametros
             nombre_funcion = elementos[0]
             nombre_autor = elementos[1]["Nombre del autor"]
             nombre_ayuda = elementos[1]["informacion de ayuda"]
             resto = elementos[1]["Resto de lineas comentadas"]
-                
+
             # Une con una coma los elementos de la lista, en una cadena nueva.
             funcion = ", ".join(resto)
-        
+
             # Genera un nombre diferente para cada funcion, para despues hacer el merge.
             archivo_a_escribir = nombre_archivo + "_" + nombre_funcion + ".csv"    
-        
+
             # Crea/abre el csv recivido por parametro.
             with open (nombre_archivo, "a") as archivo_comentetarios:
-            
+
                 #Escribo en el csv
-                archivo_comentetarios.write(nombre_funcion + "," + nombre_autor + "," + nombre_ayuda + "," + funcion + "\n")
+                archivo_comentetarios.write(nombre_funcion + "," + nombre_autor + "," + nombre_ayuda + "," + funcion + "\n") 
