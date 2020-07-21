1ef450eee02a4b4569c523b8bde05d5aa7649b27
Dan-Desktop <dan.bero15@gmail.com>
2020-07-21 01:17:18 -0300

    nada importante... : )

--- Informacion_por_desarrollador.py
+++ Informacion_por_desarrollador.py
@@ -4,10 +4,10 @@ import muestro_salida
 
 def capturo_datos():
     
-    """[Autor: Dan]"""
-    """[Ayuda: recopila los parametros para el Quinta punto
-    (Nombres de las funciones, autor de la funcion , lineas por funcion y
-    porcentaje de lineas del autor, sobre lineas totales del trabajo. ) ]
+    """ [Autor: Dan]
+        [Ayuda: recopila los parametros para el Quinta punto
+        (Nombres de las funciones, autor de la funcion , lineas por funcion y
+        porcentaje de lineas del autor, sobre lineas totales del trabajo. ) ]
     """
 
     informacion_deseada = {}
@@ -54,19 +54,19 @@ def capturo_datos():
                     informacion_deseada[clave][1] = contador_lineas
                     
                     if informacion_deseada[clave][0] in lineas_totales_por_autor.keys():
-                        lineas_totales_por_autor[informacion_deseada[clave][0]]+=contador_lineas
+                        lineas_totales_por_autor[informacion_deseada[clave][0]] += contador_lineas
                     
                     else:
-                        lineas_totales_por_autor[informacion_deseada[clave][0]]=contador_lineas
+                        lineas_totales_por_autor[informacion_deseada[clave][0]] = contador_lineas
 
             linea_archivos_fuente_unico = archivo_fuente_unico.readline()
 
     #datos porcentajes
-    porcentajes ={}
+    porcentajes = {}
 
     for clave in lineas_totales_por_autor.keys():
     
-        porcentajes[clave]=int((lineas_totales_por_autor[clave]/total_linea)*100)
+        porcentajes[clave] = int((lineas_totales_por_autor[clave]/total_linea)*100)
 
     #ordeno por autores
     
@@ -77,11 +77,12 @@ def capturo_datos():
           
 def participacion_info (informacion,informacion2):
     
-    """[Autor: Dan]"""
-    """[Ayuda: brindar datos sobre la participación de cada uno de los 
-    integrantes en el desarrollo de la aplicación.
-    Ademas de mostrar la informacion por pantalla,
-    genera la misma salida al archivo “participacion.txt” ]"""
+    """ [Autor: Dan]
+        [Ayuda: brindar datos sobre la participación de cada uno de los 
+        integrantes en el desarrollo de la aplicación.
+        Ademas de mostrar la informacion por pantalla,
+        genera la misma salida al archivo “participacion.txt” ]
+    """
 
     autor_anterior = None
     
@@ -104,15 +105,15 @@ def participacion_info (informacion,informacion2):
         if autor_anterior == None:
             
             #Muestro por pantalla...
-            s1="Autor: " + autor +"\n\n\tFuncion" + 16*" " + "Lineas"+"\n\t" + 33 * "-"
+            s1 = "Autor: " + autor + "\n\n\tFuncion" + 16*" " + "Lineas"+"\n\t" + 33 * "-"
             muestro_salida.impresiones(s1)
             
             espacios = cantidad_de_espacios(nombre_funcion)
-            s2 = "\t" + nombre_funcion + " "*espacios + str(lineas_funcion)
+            s2 = "\t" + nombre_funcion + (" " * espacios) + str(lineas_funcion)
             muestro_salida.impresiones(s2)
             
             #Agrego linea a participacion.txt
-            archivo_participacion_txt("\n"+s1)
+            archivo_participacion_txt("\n" + s1)
             archivo_participacion_txt("\n" + s2)
             
             contador_lineas_totales = 0
@@ -127,7 +128,7 @@ def participacion_info (informacion,informacion2):
 
             autor_anterior = autor
 
-        elif autor_anterior!=autor:
+        elif autor_anterior != autor:
             
             #Muestro por pantalla...
             s1 = "\t"+str(contador_funciones) + " Funciones - Lineas\t " + str(contador_lineas) + "  " + str(porcentaje_anterior)+"%\n"
@@ -138,12 +139,12 @@ def participacion_info (informacion,informacion2):
             
             #Muestro por pantalla...
             espacios = cantidad_de_espacios(nombre_funcion)
-            s3 = "\t" + nombre_funcion + " "*espacios + str(lineas_funcion)
+            s3 = "\t" + nombre_funcion + (" " * espacios) + str(lineas_funcion)
             muestro_salida.impresiones(s3)
             
             #Agrego linea a participacion.txt
             archivo_participacion_txt("\n" + s1)
-            archivo_participacion_txt("\n"+ s2)
+            archivo_participacion_txt("\n" + s2)
             archivo_participacion_txt("\n" + s3)
             
 
@@ -177,14 +178,14 @@ def participacion_info (informacion,informacion2):
         if indice == len(informacion)-1 :
             
             #Muestro por pantalla...
-            s1="\t"+str(contador_funciones) + " Funciones - Lineas\t " + str(contador_lineas) + "  " + str(porcentaje)+"%\n\n"
+            s1 = "\t"+str(contador_funciones) + " Funciones - Lineas\t " + str(contador_lineas) + "  " + str(porcentaje)+"%\n\n"
             muestro_salida.impresiones(s1)
-            s2="Total: "+ str(contador_funciones_totales) + " Funciones - lineas\t " + str(contador_lineas_totales)+"\n"
+            s2 = "Total: "+ str(contador_funciones_totales) + " Funciones - lineas\t " + str(contador_lineas_totales)+"\n"
             muestro_salida.impresiones(s2)
             
             #Agrego linea a participacion.txt
-            archivo_participacion_txt("\n"+s1)
-            archivo_participacion_txt("\n"+s2)            
+            archivo_participacion_txt("\n" + s1)
+            archivo_participacion_txt("\n" + s2)            
     
     
 
@@ -200,8 +201,11 @@ def cantidad_de_espacios(nombre_funcion):
     """
   
     if 8 + len(nombre_funcion)<33:
+        
         espacios = -1 * (8 + len(nombre_funcion) - 33)
+    
     else:
+        
         espacios = 2
 
     return espacios  
@@ -210,8 +214,9 @@ def cantidad_de_espacios(nombre_funcion):
 
 def archivo_participacion_txt (agrego):
     
-    """[Autor: Dan]"""
-    """[Ayuda: Agrega los datos generados por la funcion participacion_info]"""
+    """ [Autor: Dan]
+        [Ayuda: Agrega los datos generados por la funcion participacion_info]
+    """
 
     with open ("participacion.txt","a") as archivo_generado:
         
--- muestro_salida.py
+++ muestro_salida.py
@@ -1,8 +1,8 @@
 def impresiones (contenido_a_mostrar):
     
-    """[Autor: Dan]"""
-    """[Ayuda: Muestra por pantalla el parametro recibido]"""
+    """ [Autor: Dan]
+        [Ayuda: Muestra por pantalla el parametro recibido]
+    """
 
     print(contenido_a_mostrar)
 
-    return None
