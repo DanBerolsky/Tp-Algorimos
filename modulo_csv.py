ae8909c3795c50ce3d78c7ff6481b409521474ae
alejo-marino <tommy230201@gmail.com>
2020-07-22 22:00:56 -0300

    modulo arbol de invocacion mergeado

--- modulo_csv.py
+++ modulo_csv.py
@@ -73,3 +73,51 @@ def armo_csv(Estructura_de_datos,nombre_archivo, modulo, lista_modulos_comentari
                 archivo_comentarios.write(nombre_funcion + "," + nombre_autor + "," + nombre_ayuda + "," + funcion + "\n") 
 
         return archivo_a_escribir
+
+
+def leer_csv(nombre_csv):
+    """[Autor: F] """
+    """[Ayuda: Hace cosas] """
+    dicc_csv = {}
+    for linea in open(nombre_csv, 'r').readlines():
+        linea = linea.strip().split(',')
+        dicc_csv[linea[0]] = linea[1:]
+    return dicc_csv
+
+
+def quien_invoca_a_quien():
+    fuente = 'fuente_unico.csv'
+    # Invoco para leer el csv y que me devuelva un diccionario
+    diccionario_csv = leer_csv(fuente)
+    nombre_funcion = []
+    resultado = {}
+
+    # Me quedo con el nombre de las funciones
+    for fila in diccionario_csv:
+        nombre_funcion.append(fila)
+
+    # Recorre las filas y me quedo con la clave (nombre_funcion) y el cuerpo de la funcion
+    for fila in diccionario_csv.items():
+        clave = fila[0]
+        cuerpo = fila[1][2:]
+
+        # Por cada funcion en la lista de funciones itera
+        for funcion in nombre_funcion:
+
+            # Cruza los nombres de las funciones y chequea si esta en el cuerpo de otra funcion
+
+            funciones_en_cuerpo = [s for s in cuerpo if funcion in s]
+
+            # Cuenta cuantas veces ocurre que una funcion este dentro de ota
+            contador = len(funciones_en_cuerpo)
+            lista_de_invocacion = [funcion, contador]
+
+            # Si ocurre al menos 1 vez
+            if contador > 0:
+
+                # Si la clave ya existe, agrego otra lista, en caso de que no exista, crea la clave
+                if clave in resultado:
+                    resultado[clave].append(lista_de_invocacion)
+                else:
+                    resultado[clave] = [lista_de_invocacion]
+        return resultado
