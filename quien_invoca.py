b6980c684f68549a0a27b93670e83fa22c3aa0a8
Alfonso Campodonico <alfonso.campodonico@gmail.com>
2020-07-27 16:24:14 -0300

    Arreglos

--- quien_invoca.py
+++ quien_invoca.py
@@ -1,55 +1,42 @@
 #Declaro nombre archivo
-fuente = 'fuente_unico.csv'
+import modulo_csv
 
-def leer_csv(nombre_csv):
-    """[Autor: Alfonso]
-    [Ayuda: Recibe como parametro un string con el nombre del archivo y devuelve un diccionario con el nombre de la funcion
-    como clave principal. ] 
-    """
-        
-    dicc_csv = {}
-    
-    for linea in open(nombre_csv, 'r').readlines():
-        linea = linea.strip().split(';')
-        dicc_csv[linea[0]] = linea[1:] 
-    return dicc_csv
+fuente = 'fuente_unico.csv'
         
 def archivo_analizador(texto):
     """[Autor: Alfonso]
     [Ayuda: recibe un string como parametro y escribe en un archivo]
     """
-
     with open ("analizador.txt","a") as archivo_generado:
-        
         archivo_generado.write(texto)
-    return None
-
-
+    
 def quien_invoca_a_quien():
     """[Autor: Alfonso]
        [Ayuda: Genera un diccionario de quien inoca a quien, 
        teniendo como clave el nombre de la funcion y como valores una lista de lista con cuantas veces lo invoca, 
        ademas devuelve los nombres de todas las funciones del programa]
     """
+    
     #Invoco para leer el csv y que me devuelva un diccionario
-    diccionario_csv = leer_csv(fuente)
+    diccionario_csv = modulo_csv.leer_csv(fuente)
     nombre_funcion = []
     resultado = {}
 
-    #Me quedo con el nombre de las funciones
+    #Me quedo con el nombre de todas las funciones del archivo fuente_unico.csv
     for fila in diccionario_csv:
         nombre_funcion.append(fila)
         
     #Recorre las filas y me quedo con la clave (nombre_funcion) y el cuerpo de la funcion
     for fila in diccionario_csv.items():
+        #Me quedo con el nombre de la funcion que es la clave
         clave = fila[0]
+        #Busco el cuerpo de la funcion que seria todo lo posterior al 2do campo
         cuerpo = fila[1][2:]
         
         #Por cada funcion en la lista de funciones itera
         for funcion in nombre_funcion:
             
             #Cruza los nombres de las funciones y chequea si esta en el cuerpo de otra funcion
-            
             funciones_en_cuerpo = [s for s in cuerpo if funcion in s]
             
             #Cuenta cuantas veces ocurre que una funcion este dentro de ota 
@@ -58,14 +45,13 @@ def quien_invoca_a_quien():
             
             #Si ocurre al menos 1 vez
             if contador > 0:
-                
                 #Si la clave ya existe, agrego otra lista, en caso de que no exista, crea la clave
                 if clave in resultado:
                     resultado[clave].append(lista_de_invocacion)
-                    
+                #Si no existe la clave me creo un nuevo campo
                 else:    
                     resultado[clave] = [lista_de_invocacion]
-                    
+    #Devuelvo una lista de todas las funciones y el diccionario de quien invoca a quien       
     return nombre_funcion, resultado
  
  
@@ -82,19 +68,19 @@ def armar_tabla():
     """   
     
     lista_funciones , dicc_datos = quien_invoca_a_quien()
-    #Enumero las funciones
+    
+    #Enumero las funciones a partir de 1 numero
     enum_datos = list(enumerate(lista_funciones,1))
     
     #Posicion en la lista
     posicion = primer_item_lista(enum_datos)
     
-    #Csv de numero para el titulo
-    numeros = ','.join(map(str, posicion))
-    
     #Calculo la maxima cantidad de funciones
     maximo,titulo = formato_titulo(posicion)
+    
     lista_total = []   
     vacio = " "
+    
     #Reviso el enumerate por funcion
     for numero,funcion in enum_datos:
         
@@ -110,15 +96,23 @@ def armar_tabla():
         #Lleno la tabla con vacios
         while conteo <= maximo:
             
+            #Genero la grilla con todos los campos por funciones
             lista_a_imprimir.append(vacio)
             conteo += 1
         
-            
         for clave, valor in dicc_datos.items():
+            
+            #Reviso la lista en el cuerpo
             for lista in valor:
+                
+                #Si el nombre de la funcion esta en la lista del cuerpo
                 if funcion in lista:
+                    
+                    #Busco el numero en que fue invocada
                     numero_invocada =   [l for l in enum_datos if clave in l]
+                    #Busco el nombre de la funcion invocada
                     funcion_a_invocar = numero_invocada[0][0]
+                    #Le agrego una X a la funcion que invocaron
                     lista_a_imprimir[funcion_a_invocar] = 'X'
                     
         #Reemplazo los campos vacios por las veces que fue invocada
@@ -132,15 +126,15 @@ def armar_tabla():
                 #Calculo a cual funcion invoco basada en el numero
                 numero_invocada =   [l for l in enum_datos if i in l] 
                 
+                #Busca el nombre de la funcion invocada
                 funcion_a_invocar = numero_invocada[0][0]
                 
                 #Modifico la posicion en la lista con cuantas veces lo invoco
                 lista_a_imprimir[funcion_a_invocar] = j
-                
-       
+                 
         #Creo una lista de lista que contenga todo para calcular los totales en la matriz
         lista_total.append(lista_a_imprimir)        
-    
+
     totales = generar_totales(lista_total)
    
     return titulo, lista_total, totales
@@ -149,10 +143,10 @@ def borrar_primer_indice(lista):
     """ [Autor: Alfonso]
         [Ayuda: Borra el primer indice de una lista, acepta como parametro de entrada una lista]
     """
-    
-    for i in lista:
+    lista_2 = lista
+    for i in lista_2:
         del i[0]          
-    return lista
+    return lista_2
 
 def borrar_un_valor_lista(lista,valor):
     """ [Autor: Alfonso]
@@ -166,7 +160,6 @@ def generar_totales(lista):
     lista_totales_numero = borrar_un_valor_lista(lista,vacio)
     lista_totales_numero_2 = borrar_un_valor_lista(lista_totales_numero, 'X')   
     borrar_primer_indice(lista_totales_numero_2)
-    
     totales = sumar_totales(lista_totales_numero_2)  
     
     return totales
@@ -195,8 +188,6 @@ def formato_titulo(lista):
     
     return maximo,titulo
     
-
-
 def la_tabla():
     """ [Autor: Alfonso]
         [Ayuda: Armado de la tabla para el analizador]
@@ -223,7 +214,6 @@ def formato_tabla(lista):
     """[Autor: Alfonso]
     [Ayuda: Le da formato de tabla a una lista con espaciado y divisiones]
     """
-    
     str_nuevo = ' | '.join(map(str, lista))
     return str_nuevo
 
@@ -242,16 +232,18 @@ def correcto_espaciado(lista):
     """[Autor: Alfonso]
     [Ayuda: Modifica el espaciado de los espacios en blanco para que quede a la par cada columna despues de la decima]
     """
+    lista_1 = lista
+    
     for j,k in enumerate(lista):
             if j >= 10 and k < 10:
-               lista[j] = ' ' + str(k)
+               lista_1[j] = ' ' + str(k)
             elif j >= 10 and k >= 10:
-                lista[j] = str(k)
+                lista_1[j] = str(k)
             elif j >= 100 and k < 10:
-                lista[j] = '  ' + str(k)
+                lista_1[j] = '  ' + str(k)
             elif j >= 100 and k >= 10:
-                lista[j] = ' ' + str(k)    
-    return lista
+                lista_1[j] = ' ' + str(k)    
+    return lista_1
     
 def remover_ceros(lista):
     """[Autor: Alfonso]
